fdcfdd07aa01eefb8366a7579aefa3a6f3f3d4ef
Fisherman August <fshrmn.august@gmail.com>
2025-12-09 20:44:18 +0300

    handlers

--- main.go
+++ main.go
@@ -1,13 +1,34 @@
 package main
 
+import (
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
 /*
 	/books - get all books
 	/books/id - get book by id
 	/books - POST - create new book
 */
 
-type 
+type Books struct {
+	Id     string
+	Author string
+	Title  string
+}
+
+func getBooks(w http.ResponseWriter, r *http.Request) {
+
+}
+func createBook(w http.ResponseWriter, r *http.Request) {
+
+}
 
 func main() {
+	r := mux.NewRouter()
+	r.HandleFunc("/books", getBooks).Methods("GET")
+	r.HandleFunc("/books", createBook).Methods("POST")
 
+	http.ListenAndServe(":8080", r)
 }
